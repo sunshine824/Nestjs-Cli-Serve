325d36b1363b63de81a33d5b00e4abd9faaf9f59
sunshine824 <cx19950824>
2022-12-14 17:52:20 +0800

    代码优化

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PostsModule } from './posts/posts.module';
 import { UserModule } from './user/user.module';
 import { OrganizationModule } from './organization/organization.module';
 import { AuthModule } from './auth/auth.module';
@@ -15,7 +14,6 @@ import envConfig from '../config';
 @Module({
   imports: [
     TypeOrmModule.forRoot(envConfig.DATABASE_CONFIG),
-    PostsModule,
     UserModule,
     OrganizationModule,
     AuthModule,
